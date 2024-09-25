2f523870f93d38e6e40f831537293aab8ef3c24e
Abir Hasan Al-amin <hasan.alamin.abir+gitHUB@gmail.com>
2024-09-25 22:05:43 +0600

    Update todosSlice.js

--- frontend/src/redux/slices/todosSlice.js
+++ frontend/src/redux/slices/todosSlice.js
@@ -8,164 +8,133 @@ import {
   getSubTodos,
   updateSubTodo,
   deleteSubTodo
-} from '../../api/todoApi.js';
+} from '../../api/todoApi';
 
-export const fetchTodos = createAsyncThunk('todos/fetchTodos', async (thunkAPI) => {
-  try {
-    const todos = await getTodos();
-    return todos;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+
+const handleAsyncThunk = (builder, actionType, onSuccess) => {
+  builder
+    .addCase(actionType.pending, (state) => {
+      state.status = 'loading';
+    })
+    .addCase(actionType.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      onSuccess(state, action);
+    })
+    .addCase(actionType.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    });
+};
+
+// Async Thunks
+export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
+  const todos = await getTodos();
+  return todos;
 });
 
-export const createTodo = createAsyncThunk('todos/createTodo', async (todoData, thunkAPI) => {
-  try {
-    const todo = await addTodo(todoData);
-    return todo;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+export const createTodo = createAsyncThunk('todos/createTodo', async (todoData) => {
+  const todo = await addTodo(todoData);
+  return todo;
 });
 
-export const editTodo = createAsyncThunk('todos/editTodo', async ({ id, todoData }, thunkAPI) => {
-  try {
-    const updatedTodo = await updateTodo(id, todoData);
-    return updatedTodo;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+export const editTodo = createAsyncThunk('todos/editTodo', async ({ id, todoData }) => {
+  const updatedTodo = await updateTodo(id, todoData);
+  return updatedTodo;
 });
 
-export const removeTodo = createAsyncThunk('todos/removeTodo', async (id, thunkAPI) => {
-  try {
-    await deleteTodo(id);
-    return id;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+export const removeTodo = createAsyncThunk('todos/removeTodo', async (id) => {
+  await deleteTodo(id);
+  return id;  
 });
 
-export const createSubTodo = createAsyncThunk('todos/createSubTodo', async ({ todoId, subTodoData }, thunkAPI) => {
-  try {
-    const subTodo = await addSubTodo(todoId, subTodoData);
-    return subTodo;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+// Async thunks for subtodos
+export const createSubTodo = createAsyncThunk('todos/createSubTodo', async ({ todoId, subTodoData }) => {
+  const subTodo = await addSubTodo(todoId, subTodoData);
+  return subTodo;
 });
 
-export const fetchSubTodos = createAsyncThunk('todos/fetchSubTodos', async (todoId, thunkAPI) => {
-  try {
-    const subTodos = await getSubTodos(todoId);
-    return subTodos;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+export const fetchSubTodos = createAsyncThunk('todos/fetchSubTodos', async (todoId) => {
+  const subTodos = await getSubTodos(todoId);
+  return subTodos;
 });
 
-export const editSubTodo = createAsyncThunk('todos/editSubTodo', async ({ subTodoId, subTodoData }, thunkAPI) => {
-  try {
-    const updatedSubTodo = await updateSubTodo(subTodoId, subTodoData);
-    return updatedSubTodo;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+export const editSubTodo = createAsyncThunk('todos/editSubTodo', async ({ id, subTodoData }) => {
+  const updatedSubTodo = await updateSubTodo(id, subTodoData);
+  return updatedSubTodo;
 });
 
-export const removeSubTodo = createAsyncThunk('todos/removeSubTodo', async (subTodoId, thunkAPI) => {
-  try {
-    await deleteSubTodo(subTodoId);
-    return subTodoId;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
-  }
+export const removeSubTodo = createAsyncThunk('todos/removeSubTodo', async (id) => {
+  await deleteSubTodo(id); 
+  return id; 
 });
 
+// Redux Slice
 const todosSlice = createSlice({
   name: 'todos',
   initialState: {
     items: [],
     status: 'idle',
-    error: null, // To store any error messages
+    error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      // Fetch todos
-      .addCase(fetchTodos.fulfilled, (state, action) => {
-        state.items = action.payload;
-      })
-      .addCase(fetchTodos.rejected, (state, action) => {
-        state.error = action.payload; 
-      })
-      // Add todo
-      .addCase(createTodo.fulfilled, (state, action) => {
-        state.items.push(action.payload);
-      })
-      .addCase(createTodo.rejected, (state, action) => {
-        state.error = action.payload;
-      })
-      // Update todo
-      .addCase(editTodo.fulfilled, (state, action) => {
-        const index = state.items.findIndex((todo) => todo.id === action.payload.id);
-        if (index !== -1) {
-          state.items[index] = action.payload;
+    handleAsyncThunk(builder, fetchTodos, (state, action) => {
+      state.items = action.payload;
+    });
+
+    handleAsyncThunk(builder, createTodo, (state, action) => {
+      state.items.push(action.payload);
+    });
+
+    handleAsyncThunk(builder, editTodo, (state, action) => {
+      const index = state.items.findIndex((todo) => todo._id === action.payload._id);
+      if (index !== -1) {
+        state.items[index] = action.payload;
+      }
+    });
+
+    handleAsyncThunk(builder, removeTodo, (state, action) => {
+      state.items = state.items.filter((todo) => todo._id !== action.payload);
+    });
+
+    handleAsyncThunk(builder, fetchSubTodos, (state, action) => {
+      const index = state.items.findIndex((todo) => todo._id === action.meta.arg);
+      if (index !== -1) {
+        state.items[index].subtodos = action.payload;
+      }
+    });
+
+    handleAsyncThunk(builder, createSubTodo, (state, action) => {
+      const index = state.items.findIndex((todo) => todo._id === action.meta.arg.todoId);
+      if (index !== -1) {
+        state.items[index].subtodos.push(action.payload);
+      }
+    });
+
+    handleAsyncThunk(builder, editSubTodo, (state, action) => {
+      const todoIndex = state.items.findIndex((todo) => 
+        todo.subtodos && todo.subtodos.find((sub) => sub._id === action.payload._id)
+      );
+      if (todoIndex !== -1) {
+        const subTodoIndex = state.items[todoIndex].subtodos.findIndex(
+          (subTodo) => subTodo._id === action.payload._id
+        );
+        if (subTodoIndex !== -1) {
+          state.items[todoIndex].subtodos[subTodoIndex] = action.payload;
         }
-      })
-      .addCase(editTodo.rejected, (state, action) => {
-        state.error = action.payload; 
-      })
-      // Delete todo
-      .addCase(removeTodo.fulfilled, (state, action) => {
-        state.items = state.items.filter((todo) => todo.id !== action.payload);
-      })
-      .addCase(removeTodo.rejected, (state, action) => {
-        state.error = action.payload;
-      })
-      // Fetch subtodos
-      .addCase(fetchSubTodos.fulfilled, (state, action) => {
-        const index = state.items.findIndex((todo) => todo.id === action.meta.arg);
-        if (index !== -1) {
-          state.items[index].subtodos = action.payload;
-        }
-      })
-      .addCase(fetchSubTodos.rejected, (state, action) => {
-        state.error = action.payload;
-      })
-      // Add subtodo
-      .addCase(createSubTodo.fulfilled, (state, action) => {
-        const index = state.items.findIndex((todo) => todo.id === action.meta.arg.todoId);
-        if (index !== -1) {
-          state.items[index].subtodos.push(action.payload);
-        }
-      })
-      .addCase(createSubTodo.rejected, (state, action) => {
-        state.error = action.payload; 
-      })
-      // Update subtodo
-      .addCase(editSubTodo.fulfilled, (state, action) => {
-        const todoIndex = state.items.findIndex((todo) => todo.subtodos.some(sub => sub.id === action.payload.id));
-        if (todoIndex !== -1) {
-          const subTodoIndex = state.items[todoIndex].subtodos.findIndex(sub => sub.id === action.payload.id);
-          if (subTodoIndex !== -1) {
-            state.items[todoIndex].subtodos[subTodoIndex] = action.payload;
-          }
-        }
-      })
-      .addCase(editSubTodo.rejected, (state, action) => {
-        state.error = action.payload; 
-      })
-      // Delete subtodo
-      .addCase(removeSubTodo.fulfilled, (state, action) => {
-        const todoIndex = state.items.findIndex((todo) => todo.subtodos.some(sub => sub.id === action.payload));
-        if (todoIndex !== -1) {
-          state.items[todoIndex].subtodos = state.items[todoIndex].subtodos.filter(sub => sub.id !== action.payload);
-        }
-      })
-      .addCase(removeSubTodo.rejected, (state, action) => {
-        state.error = action.payload; 
-      });
+      }
+    });
+
+    handleAsyncThunk(builder, removeSubTodo, (state, action) => {
+      const todoIndex = state.items.findIndex((todo) => 
+        todo.subtodos && todo.subtodos.find((sub) => sub._id === action.payload)
+      );
+      if (todoIndex !== -1) {
+        state.items[todoIndex].subtodos = state.items[todoIndex].subtodos.filter(
+          (subTodo) => subTodo._id !== action.payload
+        );
+      }
+    });
   },
 });
 
